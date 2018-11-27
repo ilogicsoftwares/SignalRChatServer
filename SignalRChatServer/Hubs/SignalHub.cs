adf3a4f2f655f9beb07eb1e8a53ec386b658e898
Richard Aguirre <richardjap@gmail.com>
2018-11-27 12:25:23 -0500

    Initial commit

---
+++ SignalRChatServer/Hubs/SignalHub.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRChatServer.Hubs
+{
+    public class SignalHub:Hub
+    {
+        public static List<Client> Clientes = new List<Client>();
+
+        
+        public void Connect (string userName)
+        {
+            Client c = new Client(){
+                name=userName,
+                id=Context.ConnectionId
+
+            };
+
+            Clientes.Add(c);
+            Clients.All.updateClientes(Clientes.Count(), Clientes.Select(x => x.name));
+
+        }
+
+        public void Send (string userName, string message)
+        {
+
+            Clients.All.broadCastMessage(userName, message);
+
+        }
+
+    }
+
+
+    public class Client
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+    }
+}
---
+++ SignalRChatServer/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+[assembly: OwinStartup(typeof(SignalRChatServer.Startup))]
+namespace SignalRChatServer
+{
+    public class Startup
+    {
+
+        public void Configuration (IAppBuilder app)
+        {
+            app.MapSignalR();
+
+        }
+
+
+    }
+}
